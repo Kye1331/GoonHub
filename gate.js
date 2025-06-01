966c5e5eeb5273de8c92da7bd996be1bab132b52
Kye <kyeelde@gmail.com>
2025-06-01 14:10:32 -0700

    Update gate.js

--- gate.js
+++ gate.js
@@ -1,6 +1,3 @@
-if (localStorage.getItem('hasAccess') == 'true'){
-    window.location.href = 'niglet.html'
-}
 async function checkCode(attempt){
 let result = await fetch('https://goonhub-passcode.vercel.app/?password=' + attempt);
 let e = await result.json()
